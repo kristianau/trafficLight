6677024d83d4ace1aa4cc6ed02f5a3bcda1c2691
Kristiana Usacka <kristianausacka@gmail.com>
2023-06-24 19:39:48 +0300

    working traffic light

--- main.h
+++
@@ -1,1 +0,0 @@
-void turnOnTheLights(void);
--- trafficLight.ino
+++ trafficLight.ino
@@ -1,11 +1,47 @@
-extern "C" void turnOnTheLights(void);
+/*
+An arduino program for a mini traffic light.
+Idea is that a traffic light is in one state: green or red.
+When a button is pressed then the light changes the state.
+*/
+
+// initialize variables which represent my current pin setup on arduino
+int greenLed = 5;
+int yellowLed = 4;
+int redLed = 3;
+int button = 10;
+// for reading the states
+int buttonPressed = 0;
+int greenLedState = LOW; // a helper variable to keep track of my traffic light state
 
 void setup() {
-  // put your setup code here, to run once:
-  turnOnTheLights();
+  pinMode(greenLed, OUTPUT);
+  pinMode(yellowLed, OUTPUT);
+  pinMode(redLed, OUTPUT);
+  pinMode(button, INPUT);
+  // I'll begin my traffic light with red light being on
+  digitalWrite(redLed, HIGH);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  buttonPressed = digitalRead(button);
 
+  if(buttonPressed == HIGH && greenLedState == LOW) { // button is pressed and green is off
+    delay(200);
+    digitalWrite(redLed, LOW);
+    digitalWrite(yellowLed, HIGH);
+    delay(2000);
+    digitalWrite(yellowLed, LOW);
+    digitalWrite(greenLed, HIGH);
+    greenLedState = HIGH;
+  }
+  else if(buttonPressed == HIGH && greenLedState == HIGH) {
+    delay(200);
+    digitalWrite(greenLed, LOW);
+    digitalWrite(yellowLed, HIGH);
+    delay(2000);
+    digitalWrite(yellowLed, LOW);
+    digitalWrite(redLed, HIGH);
+    greenLedState = LOW;
+  }
 }
