477ff54272ea75a9144cfcd4317bc1f5bc22b224
Kristiana Usacka <kristianausacka@gmail.com>
2023-06-24 17:53:34 +0300

    red and yellow leds turn on/off

---
+++ trafficLight.ino
@@ -0,0 +1,11 @@
+extern "C" void turnOnTheLights(void);
+
+void setup() {
+  // put your setup code here, to run once:
+  turnOnTheLights();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
