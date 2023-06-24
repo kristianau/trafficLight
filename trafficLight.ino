7bfef9a58ac11ae108b30dae0ae7779dae86b33f
Kristiana Usacka <kristianausacka@gmail.com>
2023-06-24 20:12:32 +0300

    added comments to the program code and a picture of my project.

--- trafficLight.ino
+++ trafficLight.ino
@@ -5,10 +5,10 @@ When a button is pressed then the light changes the state.
 */
 
 // initialize variables which represent my current pin setup on arduino
-int greenLed = 5;
-int yellowLed = 4;
-int redLed = 3;
-int button = 10;
+int greenLed = 5; // pin D5 on Arduino Nano
+int yellowLed = 4; // pin D4 on Arduino Nano
+int redLed = 3; // pin D3 on Arduino Nano
+int button = 10; // pin D10 on Arduino Nano
 // for reading the states
 int buttonPressed = 0;
 int greenLedState = LOW; // a helper variable to keep track of my traffic light state
@@ -18,7 +18,7 @@ void setup() {
   pinMode(yellowLed, OUTPUT);
   pinMode(redLed, OUTPUT);
   pinMode(button, INPUT);
-  // I'll begin my traffic light with red light being on
+  // I'll begin my traffic light with red light being on:
   digitalWrite(redLed, HIGH);
 }
 
@@ -28,18 +28,22 @@ void loop() {
 
   if(buttonPressed == HIGH && greenLedState == LOW) { // button is pressed and green is off
     delay(200);
+    // turn on yellow
     digitalWrite(redLed, LOW);
     digitalWrite(yellowLed, HIGH);
     delay(2000);
+    // turn on green
     digitalWrite(yellowLed, LOW);
     digitalWrite(greenLed, HIGH);
     greenLedState = HIGH;
   }
   else if(buttonPressed == HIGH && greenLedState == HIGH) {
     delay(200);
+    // turn on yellow
     digitalWrite(greenLed, LOW);
     digitalWrite(yellowLed, HIGH);
     delay(2000);
+    // turn on red
     digitalWrite(yellowLed, LOW);
     digitalWrite(redLed, HIGH);
     greenLedState = LOW;
